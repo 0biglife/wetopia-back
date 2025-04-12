012c04b0fabeeb54e27aa133f2275c0933e5853f
0biglife <0biglife@gmail.com>
2025-04-12 23:07:48 +0900

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://0biglife.com'],
+    credentials: true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   // const seeder = app.get(SeederService);
   // await seeder.seedAllSymbols(STOCK_SYMBOLS);
 
